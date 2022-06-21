862a630deb4ce334e93d72fbc59e8662ae7276ed
krause_a_v@mail.ru <Fktrctqr3377>
2022-06-21 19:55:28 +0600

    buscompany(save)

---
+++ src/main/java/com/akrauze/buscompany/controllers/AccountController.java
@@ -0,0 +1,9 @@
+package com.akrauze.buscompany.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+}
---
+++ src/main/java/com/akrauze/buscompany/controllers/OrderController.java
@@ -0,0 +1,9 @@
+package com.akrauze.buscompany.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+}
