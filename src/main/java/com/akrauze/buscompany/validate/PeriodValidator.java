935ef2841df10518ddbe231474b9dd928fe06c15
krause_a_v@mail.ru <Fktrctqr3377>
2022-07-23 12:26:26 +0600

    buscompany(save)

--- src/main/java/com/akrauze/buscompany/validate/PeriodValidator.java
+++ src/main/java/com/akrauze/buscompany/validate/PeriodValidator.java
@@ -10,25 +10,35 @@ public class PeriodValidator implements ConstraintValidator<PeriodValid, String>
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (s == null || s.equals(""))
+            return false;
         boolean result = false;
         List<String> periods = Arrays.asList(s.split(","));
         List<String> periodsRes = periods.stream()
-                .filter(p -> p.equalsIgnoreCase("daily"))
-                .filter(p -> p.equalsIgnoreCase("even"))
-                .filter(p -> p.equalsIgnoreCase("odd")).collect(Collectors.toList());
-        if (periodsRes.size() != 0)
-            result = true;
+                .filter(p -> p.equalsIgnoreCase("daily")
+                        || p.equalsIgnoreCase("even")
+                        || p.equalsIgnoreCase("odd"))
+                .collect(Collectors.toList());
+        if (periodsRes.size() == 1) {
+            return true;
+        }
+        else if(periodsRes.size() > 1) {
+            return false;
+        }
         if (periodsRes.size()==0) {
             periodsRes = periods.stream()
-                    .filter(p -> p.equals("Sun"))
-                    .filter(p -> p.equals("Mon"))
-                    .filter(p -> p.equals("Tue"))
-                    .filter(p -> p.equals("Wed"))
-                    .filter(p -> p.equals("Thu"))
-                    .filter(p -> p.equals("Fri"))
-                    .filter(p -> p.equals("Sat")).collect(Collectors.toList());
-            if(periodsRes.size()!=0)
+                    .filter(p -> p.equals("Sun")
+                            || p.equals("Mon")
+                            || p.equals("Tue")
+                            || p.equals("Wed")
+                            || p.equals("Thu")
+                            || p.equals("Fri")
+                            || p.equals("Sat"))
+                    .collect(Collectors.toList());
+            if(periodsRes.size()!=0 && periodsRes.size()==periods.size())
                 result = true;
+            else if (periodsRes.size()!=0 && periodsRes.size()!=periods.size())
+                return false;
         }
         if (periodsRes.size()==0) {
             for (String d : periods) {
